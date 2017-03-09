0b18024dba4259e11506362c01f2f1374fa5edd4
Hutchison <ghutchison2600@MAC-P1JWF8J4.local>
2017-03-09 14:35:47 -0500

    Basic App Started

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var awesomeSlider: UISlider!
+    @IBOutlet weak var awesomeLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    let twoThird: Float = 0.66, oneThird: Float = 0.33;
+    @IBAction func awesomeUpdate(sender: AnyObject) {
+        var slider: UISlider = awesomeSlider;
+        var output = nameTextField.text + " ";
+        if (slider.value > twoThird) {
+            output += "And very much awesome.";
+        } else if (slider.value > oneThird) {
+            output += "Not much but yeah some.";
+        } else {
+            output += "Is just not awesome :(";
+        }
+        awesomeLabel.text = output;
+    }
+    @IBAction func hideKeyboard(sender: AnyObject) {
+        self.view.endEditing(true);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
